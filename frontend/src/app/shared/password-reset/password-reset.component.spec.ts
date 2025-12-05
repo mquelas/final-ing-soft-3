a45b5157fd566e6eeaae2e3cd3503e5c12005c2e
mquelas <maxiquelas80@gmail.com>
2025-12-05 16:01:17 -0300

    Fix password reset spec dependencies and install pytest

--- frontend/src/app/shared/password-reset/password-reset.component.spec.ts
+++ frontend/src/app/shared/password-reset/password-reset.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { PasswordResetComponent } from './password-reset.component';
 
 describe('ResetPasswordComponent', () => {
@@ -8,7 +9,7 @@ describe('ResetPasswordComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PasswordResetComponent, RouterTestingModule],
+      imports: [PasswordResetComponent, RouterTestingModule, HttpClientTestingModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(PasswordResetComponent);
