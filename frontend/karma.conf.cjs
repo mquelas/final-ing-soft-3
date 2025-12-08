570f3e6cb5984c62744b264b7a5afd8e720f7ddc
Josefina Rivero <josefinarivero03@gmail.com>
2025-12-08 13:15:58 -0300

    arreglo de pipeline

---
+++ frontend/karma.conf.cjs
@@ -0,0 +1,34 @@
+// Karma configuration with coverage thresholds to enforce quality gates in CI
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    browsers: ['ChromeHeadless'],
+    singleRun: true,
+    reporters: ['progress', 'coverage'],
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      reporters: [
+        { type: 'lcov', subdir: '.' },
+        { type: 'text-summary' }
+      ],
+      check: {
+        global: {
+          statements: 70,
+          branches: 70,
+          functions: 70,
+          lines: 70
+        }
+      }
+    },
+    client: {
+      clearContext: false
+    }
+  });
+};
