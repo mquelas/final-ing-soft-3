513e431c10322c4e3b80b0c1fe8181cee5bbc357
Josefina Rivero <josefinarivero03@gmail.com>
2025-12-09 14:36:18 -0300

    fallo unit tests

--- backend/tests/test_admin_users_routes.py
+++ backend/tests/test_admin_users_routes.py
@@ -182,7 +182,7 @@ def test_create_and_list_companies(admin_client):
             "cuil": 7777,
             "nombre": "Nueva",
             "rubro": "IT",
-            "cant_empleados": 10,
+            "cant_empleados": "hola",
             "fecha_ingreso": "2023-01-01",
             "horario_trabajo": "09-18",
             "estado": True,
